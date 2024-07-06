f00da0182b9483272ccbbad0d946e3e4e7049e1d
amir.hesari1848@gmail.com <amir.hesari1848@gmail.com>
2024-07-06 22:00:15 +0330

    front

--- Adoo/Program.cs
+++ Adoo/Program.cs
@@ -10,12 +10,17 @@ namespace Adoo
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "wwwroot"; // Path to your Angular build output
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -27,6 +32,20 @@ namespace Adoo
 
 
             app.MapControllers();
+
+            app.MapWhen(context => !context.Request.Path.StartsWithSegments("/api"),
+                    builder =>
+                    {
+                        builder.UseSpa(spa =>
+                        {
+                            spa.Options.SourcePath = "wwwroot"; // Path to your Angular project directory
+
+                            //if (env.IsDevelopment())
+                            //{
+                            //    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200"); // If using Angular CLI development server
+                            //}
+                        });
+                    });
 
             app.Run();
         }
